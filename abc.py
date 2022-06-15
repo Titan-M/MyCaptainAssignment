33f8e9d121a0179b1250fd1aec939a257cb990a9
Titan-M <100458631+Titan-M@users.noreply.github.com>
2022-06-15 21:15:00 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -2,3 +2,6 @@ PI = 3.14
 r=float(input("Enter the radius of the circle: "))
 area = PI * r* r
 print("Area of the circle = %.2f" %area)
+import os
+file_name = 'abc.py'
+print(os.path.splitext(file_name))
